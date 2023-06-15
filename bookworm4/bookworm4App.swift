82965c23063e3fe9a2eaa26b274cd579cf8d43a9
pegahtafviziingoampt <ingoampt@gmail.com>
2023-06-15 16:43:42 +0200

    Initial Commit

---
+++ bookworm4/bookworm4App.swift
@@ -0,0 +1,17 @@
+//
+//  bookworm4App.swift
+//  bookworm4
+//
+//  Created by Pegah Tafvizi on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct bookworm4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
